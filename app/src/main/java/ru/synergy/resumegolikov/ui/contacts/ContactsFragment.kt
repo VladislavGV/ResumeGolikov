1fc30dc95e70bd626230ffe0b485dbbef525db62
VladislavGV <0377722@mail.ru>
2022-10-06 13:33:11 +0300

    Finished source

--- app/src/main/java/ru/synergy/resumegolikov/ui/contacts/ContactsFragment.kt
+++ app/src/main/java/ru/synergy/resumegolikov/ui/contacts/ContactsFragment.kt
@@ -3,16 +3,21 @@ package ru.synergy.resumegolikov.ui.contacts
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import ru.synergy.resumegolikov.databinding.FragmentContactsBinding
+import ru.synergy.resumegolikov.databinding.FragmentHomeBinding
+import ru.synergy.resumegolikov.ui.home.HomeViewModel
 
 
 class ContactsFragment : Fragment() {
@@ -26,61 +31,31 @@ class ContactsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         val contactsViewModel =
             ViewModelProvider(this).get(ContactsViewModel::class.java)
 
         _binding = FragmentContactsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val textView: TextView = binding.contactsPhone
+        textView.movementMethod = LinkMovementMethod.getInstance()
 
-        val messageEditText: EditText = binding.message
+        val whatsTextView: TextView = binding.whatsapp
+        whatsTextView.movementMethod = LinkMovementMethod.getInstance()
 
-        val submit: Button = binding.submit
+        val tlgTextView: TextView = binding.telegram
+        tlgTextView.movementMethod = LinkMovementMethod.getInstance()
 
-        fun sendMessage(message: String) {
 
-            // Creating intent with action send
-            val intent = Intent(Intent.ACTION_SEND)
+        return root
+    }
 
-            // Setting Intent type
-            intent.type = "text/plain"
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
-            // Setting whatsapp package name
-            intent.setPackage("com.whatsapp")
 
-            // Give your message here
-            intent.putExtra(Intent.EXTRA_TEXT, message)
-
-            // Checking whether whatsapp is installed or not
-            fun isAppEnabled(packageName: String, context: Context): Boolean {
-                return try {
-                    val packageManager = context.packageManager
-                    packageManager.getApplicationInfo(packageName, 0).enabled
-                } catch (e: PackageManager.NameNotFoundException) {
-                    false
-                }
-            }
-
-//            if (intent.resolveActivity(packageManager) == null) {
-//                Toast.makeText(
-//                    this@ContactsFragment.requireActivity(), "Сначала установите WhatsApp",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//                return
-//            }
-
-            // Starting Whatsapp
-            startActivity(intent)
-        }
-            return root
-        }
-
-        override fun onDestroyView() {
-            super.onDestroyView()
-            _binding = null
-
-        }
 }
 
 
--- app/src/main/java/ru/synergy/resumegolikov/ui/contacts/ContactsViewModel.kt
+++ app/src/main/java/ru/synergy/resumegolikov/ui/contacts/ContactsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ContactsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        println(text)
     }
     val text: LiveData<String> = _text
 }
